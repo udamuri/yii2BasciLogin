9dc6f82e81d730f79574e8837905d76bcfb4ff93
udamuri <udamuri@gmail.com>
2019-11-22 09:00:51 +0700

    first commit

---
+++ migrations/m191122_013421_init.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191122_013421_init
+ */
+class m191122_013421_init extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        
+        $this->createTable('{{%user}}', [
+            'id' => $this->bigPrimaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'firstname' => $this->string(100)->notNull(),
+            'lastname' => $this->string(100),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->insert('user', [
+            'username'=> 'admin',
+            'firstname'=> 'Admin',
+            'lastname'=> '',
+            'auth_key'=> \Yii::$app->security->generateRandomString(),
+            'password_hash'=> \Yii::$app->getSecurity()->generatePasswordHash('123456'),
+            'password_reset_token'=> NULL,
+            'created_at' => strtotime(date('Y-m-d H:i:s')),
+            'updated_at' => strtotime(date('Y-m-d H:i:s')),
+            'email'=> 'sholihatwarda@gmail.com',
+            'status'=> 10,
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
